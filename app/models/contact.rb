95d4e8500c0730446d57633bb941270ce170cab0
Keegan Pasqualoni <keeganpeaco@yahoo.com>
2026-04-28 16:29:58 -0600

    Fixed potentially unsafe URL

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,15 @@
 class Contact < ApplicationRecord
   has_one_attached :logo
 
+  def safe_link_url
+    if link_url != nil
+      uri = URI.parse(link_url)
+      return link_url if %w[http https].include?(uri.scheme)
+      nil
+    end
+  rescue URI::InvalidURIError
+    nil
+  end
+
   validates :logo, presence: true
 end
