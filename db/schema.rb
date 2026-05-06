0ae7a493428607265f1caf4c7db8c9132ed1d38f
Keegan Pasqualoni <keeganpeaco@yahoo.com>
2026-05-06 09:25:43 -0600

    Renamed ContactMethods back to Contact for future use in mailer form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_30_204008) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_06_151601) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
@@ -37,14 +37,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_30_204008) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-  end
-
-  create_table "contact_methods", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.string "link_url"
-    t.string "service_name"
-    t.datetime "updated_at", null: false
-    t.string "username"
   end
 
   create_table "projects", force: :cascade do |t|
--- test/models/contact_method_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class ContactMethodTest < ActiveSupport::TestCase
-  test "should not save new contact_method without logo" do
-    contact = ContactMethod.new(username: "Test username", service_name: "Test service name")
-    assert_not contact.save
-  end
-end
